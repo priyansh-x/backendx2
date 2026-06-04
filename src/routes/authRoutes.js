840f4339f779ba4df1107100bf2d426c1eca2167
Fugazi0 <priyansh.joshix77@gmail.com>
2026-06-04 23:21:35 +0530

    console view of login details

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,7 +4,14 @@ import jwt from 'jsonwebtoken'
 import db from '../db.js'
 
 const router = express.Router()
-router.post('/register' , (req,res) => {})
+router.post('/register' , (req,res) => {
+    const {username, password} = req.body
+    console.log(username,password)
+    res.sendStatus(201)
+
+})
+
+
 router.post('/login', (req,res) => {})
 
 
