aef076bf63fae721d2f6bc04793e3b11947f5ef2
Fugazi0 <priyansh.joshix77@gmail.com>
2026-06-04 23:06:50 +0530

    backend attempt

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { metadata } from 'framer-motion/client'
+import path, { dirname } from 'path'
+import { fileURLToPath  } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
+
+const app = express()
+const PORT = process.env.PORT || 8080 
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, 'public' , 'index.html'))
+})
+
+app.use('/auth', authRoutes)
+app.use('/todos', todoRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`server has started on : ${PORT}`)
+})
