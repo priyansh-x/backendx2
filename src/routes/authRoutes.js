b91fc78d9c7ac8e76d0af62e0216ea8eaf221ef8
Fugazi0 <priyansh.joshix77@gmail.com>
2026-06-05 13:37:21 +0530

    /auth/login and jsw token setup.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,13 +6,32 @@ import db from '../db.js'
 const router = express.Router()
 router.post('/register' , (req,res) => {
     const {username, password} = req.body
-    console.log(username,password)
-    res.sendStatus(201)
+    //encrypt pwd
+    const hashedPassword = bcrypt.hashSync(password,8)
+    try{
+        const insertUser = db.prepare(`INSERT INTO users (username,password) VALUES (?,?)`)
+        const result = insertUser.run(username,hashedPassword)
+
+        const defaultTodo = `Wsup, add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id,task) VALUES (?,?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn:'24h'})
+        res.json({token})
+
+    } catch (err){
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+    console.log(username,hashedPassword)
 
 })
 
 
-router.post('/login', (req,res) => {})
+router.post('/login', (req,res) => {
+
+
+})
 
 
 export default router
