11581701629d41bf4f1d76f5152b5ac55cec1d8d
Fugazi0 <priyansh.joshix77@gmail.com>
2026-06-06 12:30:35 +0530

    generated and logged in token for user ids

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+const { JsonWebTokenError } = jwt
 import db from '../db.js'
+
 
 const router = express.Router()
 router.post('/register' , (req,res) => {
@@ -29,7 +31,25 @@ router.post('/register' , (req,res) => {
 
 
 router.post('/login', (req,res) => {
+    const{username, password} = req.body
 
+    try{    
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser.get(username)
+        if(!user){return res.status(404).send({message:"User not found"})}
+
+        const passwordIsValid = bcrypt.compareSync(password,user.password)
+        if(!passwordIsValid) { return res.sendStatus(401).send({message:"password is invalid brev"})}
+        console.log(user)
+       
+        const token = jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'24h'})
+        res.json({ token })
+
+
+    }catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 
 })
 
