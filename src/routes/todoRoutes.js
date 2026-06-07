1db369e583af15f3011410dd627b302e85fe718f
Fugazi0 <priyansh.joshix77@gmail.com>
2026-06-07 17:20:00 +0530

    todo completed working

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -10,19 +10,23 @@ router.get('/', (req,res) => {
 
 })
 
-router.post('/', (req,res) => {
+router.post('/', (req,res) => { 
+    const {task} = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id,task) VALUES (?,?)`)
+    const result = insertTodo.run(req.userId, task) 
 
-
-})
-
-router.put('/', (req,res) => {
-
+    res.json({id: result.lastInsertRowid , result, task, completed: 0})
 
 })
 
 router.put('/:id', (req,res) => {
+    const { completed } = req.body
+    const { id } = req.params
 
 
+    const updateTodo = db.prepare('UPDATE Todos SET Completed = ? ')
+    updateTodo.run(completed,id)
+    res.json({message:"todo completed"})
 })
 
 router.delete('/:id', (req,res) => {
